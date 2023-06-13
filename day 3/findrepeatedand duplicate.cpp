0b8650027c85d8ebfa70d98418f03660425c5a6f
AnantkrTete <135524288+AnantkrTete@users.noreply.github.com>
2023-06-13 17:13:16 +0530

    Update findrepeatedand duplicate.cpp

--- day 3/findrepeatedand duplicate.cpp
+++ day 3/findrepeatedand duplicate.cpp
@@ -28,4 +28,24 @@ vector<int> Solution::repeatedNumber(const vector<int> &A) {
 
     return result;
 }
+//optimal approach below using maths
+vector<int> Solution::repeatedNumber(const vector<int> &a) {
+    long long n=a.size();
+    long long sn;
+    sn=(n*(n+1))/2;
+    long long sn2;
+    sn2=(n*(n+1)*((2*n)+1))/6;
+    long long s=0, s2=0;
+    long long val1;
+    long long val2;
+    for(int i=0;i<n;i++){
+        s+=a[i];
+        s2+=(long long)a[i]*(long long)a[i];
+    }
+    val1=s-sn;
+    val2=(s2-sn2)/val1;
+    long long x=(val1+val2)/2;
+    long long y=val2-x;
+    return {x,y};
+    }
 
